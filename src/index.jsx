49b3f73784d056451a4b8beb003f120a09f61404
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2024-01-15 21:53:37 +0900

    Updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,5 @@
-import { createRoot } from 'react-dom/client';
-<<<<<<< Updated upstream
 import React from 'react';
-import './index.scss';
-
-const MyFlixApplication = () => {
-  return (
-    <div className='my-flix'>
-      <div>Good morning</div>
-    </div>
-  );
-};
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-=======
+import { createRoot } from 'react-dom/client';
 import "./index.scss";
 import MainView from "./components/main-view/main-view";
 
@@ -31,5 +17,5 @@ const MyFlixApplication = () => {
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
->>>>>>> Stashed changes
+
 root.render(<MyFlixApplication />);
