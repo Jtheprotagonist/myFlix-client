fbd6a82d35bb863859da3859c7217dbaa4f6ea9d
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2024-01-21 18:47:14 +0900

    Updated components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,37 +1,3 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-const MovieCard = ({ movie, onMovieClick }) => {
-  return (
-    <div
-      style={{
-        border: "1px solid #ccc",
-        padding: "10px",
-        margin: "10px",
-        cursor: "pointer",
-      }}
-      onClick={() => onMovieClick(movie)}
-    >
-      <img
-        src={movie.image}
-        alt={movie.title}
-        style={{ maxWidth: "100%", height: "auto" }}
-      />
-      <h3>{movie.title}</h3>
-    </div>
-  );
+export const BookCard = () => {
+  return <div>some title</div>;
 };
-
-// PropTypes validation
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
-    // Add other movie properties as needed
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
-};
-
-export default MovieCard;
