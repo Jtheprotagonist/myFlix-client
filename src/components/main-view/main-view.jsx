ec918b7441556fe4773a8f12310fc2c102227b88
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2024-03-23 09:03:48 +0900

    Update components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import MovieView from "../movie-view/movie-view";
+import SignupView from "../signup-view/signup-view";
 
 const MainView = () => {
   const [movies, setMovies] = useState([]); // State variable to store movies
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-  fetch('https://movie-murmer-2015-5d256703e312.herokuapp.com/movies')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Unauthorized');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log('Data received:', data);
-      setMovies(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      // Handle the error, e.g., display a message to the user
-    });
-}, []);
+    fetch('https://movie-murmer-2015-5d256703e312.herokuapp.com/movies')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Unauthorized');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Data received:', data);
+        setMovies(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        // Handle the error, e.g., display a message to the user
+      });
+  }, []);
 
-  
+  const handleLogout = () => {
+    // Perform logout logic here
+    console.log("Logout clicked");
+    // Example: Clear authentication token, reset user session, etc.
+  };
 
   const handleCardClick = (clickedMovie) => {
     setSelectedMovie(clickedMovie);
@@ -42,8 +48,24 @@ const MainView = () => {
     return <div>The list is empty!</div>;
   }
 
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
+
   return (
     <div>
+      {/* Logout button */}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+
       {movies.map((movie) => (
         <MovieCard
           key={movie._id} // Assuming the id property is named "_id", adjust if necessary
